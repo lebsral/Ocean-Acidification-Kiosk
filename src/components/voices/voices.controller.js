8e1028db8b909d1bcc848ddc0748597aa1bd7c8f
Lars Bell <larsbell@gmail.com>
2015-03-25 23:57:55 -0800

    adds basic structure of voices

---
+++ src/components/voices/voices.controller.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('acidification')
+  .controller('VoicesController', function ($sce, $timeout) {
+            var controller = this;
+            controller.state = null;
+            controller.API = null;
+            controller.currentVideo = 0;
+
+  });
