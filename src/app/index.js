e40eff5969f11ee5431585ac0ffa44e4068c3ebc
Lars Bell <larsbell@gmail.com>
2015-03-26 12:28:54 -0800

    puts in the video di

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,21 @@
 'use strict';
 
-angular.module('acidification', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'ngMaterial'])
+angular.module('acidification', [
+		'ngAnimate',
+		'ngCookies',
+		'ngTouch',
+		'ngSanitize',
+		'ngResource',
+		'ui.router',
+		'ngMaterial',
+		'angulartics',
+		'angulartics.google.analytics',
+		'com.2fdevs.videogular',
+		'com.2fdevs.videogular.plugins.controls',
+		'com.2fdevs.videogular.plugins.overlayplay',
+		'com.2fdevs.videogular.plugins.poster'
+		])
+
   .config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
       .state('home', {
