60944c6eb3ef0632ae7a58930576f1f5308a234b
Lars Bell <larsbell@gmail.com>
2015-04-09 16:46:27 -0800

    fixes jshint problems

--- src/app/index.js
+++ src/app/index.js
@@ -1,22 +1,22 @@
-'use strict';
 
 angular.module('acidification', [
-		'ngAnimate',
-		'ngCookies',
-		'ngTouch',
-		'ngSanitize',
-		'ngResource',
-		'ui.router',
-		'ngMaterial',
-		//'angulartics',
-		//'com.2fdevs.videogular.analytics',
-		'com.2fdevs.videogular',
-		'com.2fdevs.videogular.plugins.controls',
-		'com.2fdevs.videogular.plugins.overlayplay',
-		'com.2fdevs.videogular.plugins.poster'
-		])
+  'ngAnimate',
+  'ngCookies',
+  'ngTouch',
+  'ngSanitize',
+  'ngResource',
+  'ui.router',
+  'ngMaterial',
+  //'angulartics',
+  //'com.2fdevs.videogular.analytics',
+  'com.2fdevs.videogular',
+  'com.2fdevs.videogular.plugins.controls',
+  'com.2fdevs.videogular.plugins.overlayplay',
+  'com.2fdevs.videogular.plugins.poster'
+])
 
   .config(function ($stateProvider, $urlRouterProvider) {
+    'use strict';
     $stateProvider
       .state('home', {
         url: '/',
