1f6af62aff6989dae2c059dd2c1f3789c5af0046
Lars Bell <larsbell@gmail.com>
2015-04-30 11:38:57 -0800

    o videogular: you suck

--- src/app/index.js
+++ src/app/index.js
@@ -13,58 +13,56 @@ angular.module('acidification', [
   'com.2fdevs.videogular.plugins.controls',
   'com.2fdevs.videogular.plugins.overlayplay',
   'com.2fdevs.videogular.plugins.poster'
-
 ])
 
-.run(['$rootScope', '$location', '$window', function ($rootScope, $location, $window) {
-  $rootScope
-    .$on('$stateChangeSuccess',
-      function (event) {
-
-        if (!$window.ga)
+  .run(['$rootScope', '$location', '$window', function ($rootScope, $location, $window) {
+    'use strict';
+    $rootScope
+      .$on('$stateChangeSuccess',
+        function (event) {
+          if (!$window.ga)
           return;
-
-        $window.ga('send', 'pageview', {
-          page: $location.path()
+          $window.ga('send', 'pageview', {
+            page: $location.path()
+          });
         });
+  }])
+
+  .config(function ($stateProvider, $urlRouterProvider) {
+    'use strict';
+
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      })
+      .state('what-is-oa', {
+        url: '/what-is-oa',
+        templateUrl: 'components/what-is-oa/partial-what-is-oa.html'
+      })
+      .state('act-now', {
+        url: '/act-now',
+        templateUrl: 'components/act-now/partial-act-now.html'
+      })
+      .state('look-closer', {
+        url: '/look-closer',
+        templateUrl: 'components/look-closer/partial-look-closer.html'
+      })
+      .state('oa-facts', {
+        url: '/oa-facts',
+        templateUrl: 'components/oa-facts/partial-oa-facts.html'
+      })
+      .state('voices', {
+        url: '/voices',
+        templateUrl: 'components/voices/partial-voices.html',
+        controller: 'VoicesCtrl'
+      })
+      .state('voices.fishermen', {
+        templateUrl: 'partial-voices-fishermen.html',
+        controller: function ($scope) {
+          $scope.fishermen = ['Bob', 'Sue', 'James'];
+        }
       });
-}])
-
-.config(function ($stateProvider, $urlRouterProvider) {
-  'use strict';
-
-  $stateProvider
-    .state('home', {
-      url: '/',
-      templateUrl: 'app/main/main.html',
-      controller: 'MainCtrl'
-    })
-    .state('what-is-oa', {
-      url: '/what-is-oa',
-      templateUrl: 'components/what-is-oa/partial-what-is-oa.html'
-    })
-    .state('act-now', {
-      url: '/act-now',
-      templateUrl: 'components/act-now/partial-act-now.html'
-    })
-    .state('look-closer', {
-      url: '/look-closer',
-      templateUrl: 'components/look-closer/partial-look-closer.html'
-    })
-    .state('oa-facts', {
-      url: '/oa-facts',
-      templateUrl: 'components/oa-facts/partial-oa-facts.html'
-    })
-    .state('voices', {
-      url: '/voices',
-      templateUrl: 'components/voices/partial-voices.html',
-      controller: 'VoicesCtrl'
-    })
-    .state('voices.fishermen', {
-      templateUrl: 'partial-voices-fishermen.html',
-      controller: function ($scope) {
-        $scope.fishermen = ['Bob', 'Sue', 'James'];
-      }
-    });
-  $urlRouterProvider.otherwise('/');
-});
+    $urlRouterProvider.otherwise('/');
+  });
--- src/components/voices/voices.controller.js
+++ src/components/voices/voices.controller.js
@@ -1,10 +1,4 @@
-angular.module('acidification', [
-  'ngSanitize',
-  'com.2fdevs.videogular',
-  'com.2fdevs.videogular.plugins.controls',
-  'com.2fdevs.videogular.plugins.overlayplay',
-  'com.2fdevs.videogular.plugins.poster'
-])
+angular.module('acidification')
 
 
   .controller('VoicesCtrl',
@@ -16,7 +10,7 @@ angular.module('acidification', [
 				  {src: $sce.trustAsResourceUrl('http://static.videogular.com/assets/videos/videogular.mp4'), type: 'video/mp4'},
 				  {src: $sce.trustAsResourceUrl('http://static.videogular.com/assets/videos/videogular.webm'), type: 'video/webm'},
 				  {src: $sce.trustAsResourceUrl('http://static.videogular.com/assets/videos/videogular.ogg'), type: 'video/ogg'}
-			],
+			  ],
 		    tracks: [
 				  {
 					  src: 'http://www.videogular.com/assets/subs/pale-blue-dot.vtt',
@@ -24,12 +18,12 @@ angular.module('acidification', [
 					  srclang: 'en',
 					  label: 'English',
 					  default: ''
-				}
-			],
-			theme: 'bower_components/videogular-themes-default/videogular.css',
-			plugins: {
-				poster: 'http://www.videogular.com/assets/images/videogular.png'
-			}
-		};
-	}]
-);
+				  }
+        ],
+        theme: 'bower_components/videogular-themes-default/videogular.css',
+		    plugins: {
+		      poster: 'http://www.videogular.com/assets/images/videogular.png'
+        }
+		  };
+    }]
+             );
