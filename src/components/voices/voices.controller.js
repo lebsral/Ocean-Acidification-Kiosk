0bd50b73c21626047fd1773fe5207c397f5b6cac
Lars Bell <larsbell@gmail.com>
2015-05-01 11:27:59 -0800

    added video css

--- src/components/voices/voices.controller.js
+++ src/components/voices/voices.controller.js
@@ -6,24 +6,24 @@ angular.module('acidification')
     ['$sce', function ($sce) {
       'use strict';
 		  this.config = {
-        sources: [
-				  {src: $sce.trustAsResourceUrl('http://static.videogular.com/assets/videos/videogular.mp4'), type: 'video/mp4'},
-				  {src: $sce.trustAsResourceUrl('http://static.videogular.com/assets/videos/videogular.webm'), type: 'video/webm'},
-				  {src: $sce.trustAsResourceUrl('http://static.videogular.com/assets/videos/videogular.ogg'), type: 'video/ogg'}
-			  ],
-		    tracks: [
-				  {
-					  src: 'http://www.videogular.com/assets/subs/pale-blue-dot.vtt',
-					  kind: 'subtitles',
-					  srclang: 'en',
-					  label: 'English',
-					  default: ''
-				  }
-        ],
-        theme: 'bower_components/videogular-themes-default/videogular.css',
-		    plugins: {
-		      poster: 'http://www.videogular.com/assets/images/videogular.png'
-        }
+			sources: [
+				{src: $sce.trustAsResourceUrl('assets/videos/Dune.mp4'), type: 'video/mp4'}
+				// {src: $sce.trustAsResourceUrl('http://static.videogular.com/assets/videos/videogular.mp4'), type: 'video/mp4'}
+				// {src: $sce.trustAsResourceUrl('http://static.videogular.com/assets/videos/videogular.webm'), type: 'video/webm'},
+				// {src: $sce.trustAsResourceUrl('http://static.videogular.com/assets/videos/videogular.ogg'), type: 'video/ogg'}
+			],
+			tracks: [
+				{
+					src: 'http://www.videogular.com/assets/subs/pale-blue-dot.vtt',
+					kind: 'subtitles',
+					srclang: 'en',
+					label: 'English'
+				}
+			],
+			theme: 'bower_components/videogular-themes-default/videogular.css',
+			plugins: {
+				poster: 'http://www.videogular.com/assets/images/videogular.png'
+			}
 		  };
     }]
              );
