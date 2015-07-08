c12c3c48da1798df08837c66d66f1fe524788a02
Lars Bell <larsbell@gmail.com>
2015-07-07 16:18:22 -0800

    adds DI for videogular

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular
-    .module('acidification', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'ngMaterial']);
+    .module('acidification', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'ngMaterial', 'com.2fdevs.videogular', 'com.2fdevs.videogular.plugins.controls', 'com.2fdevs.videogular.plugins.overlayplay', 'com.2fdevs.videogular.plugins.poster']);
 
 })();
