f3ee3cd95e5a2d47612bfc03839fbfc2f34fdefd
Lars Bell <larsbell@gmail.com>
2015-07-13 15:07:11 -0800

    adds checkboxes to act now

--- src/app/actnow/controllers/actnow-controller.js
+++ src/app/actnow/controllers/actnow-controller.js
@@ -9,19 +9,24 @@
 angular.module('acidification')
   .controller('actnowCtrl', function ($scope, $firebaseArray) {
 
+    $scope.newOAList = true;
+
+    $scope.checkboxModel = {
+      newOAList : true,
+      newCoalList : false,
+      newLegList: false
+    };
 
     $scope.place = null;
     $scope.autocompleteOptions = {
       types: ['cities']
     };
 
-
-
     var ref = new Firebase('https://oceanacidification.firebaseio.com/');
     $scope.emails = $firebaseArray(ref);
 
   // provide a method for adding an email
-    $scope.addEmail = function (newEmail, newFirstName, newLastName, newCity, newPlace) {
+    $scope.addEmail = function (newEmail, newFirstName, newLastName, newCity, newPlace, newOAList) {
       if (newEmail) {
         // push a message to the end of the array
         $scope.emails.$add({
@@ -29,7 +34,8 @@ angular.module('acidification')
           firstName: newFirstName,
           lastName: newLastName,
           city: newCity,
-          place: newPlace
+          place: newPlace,
+          OAList: newOAList
 
         });
 
