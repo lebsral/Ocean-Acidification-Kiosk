1d1286ccbf968b0e0e12950799aae4ab36f0a3fd
Lars Bell <larsbell@gmail.com>
2015-07-14 14:14:30 -0800

    makes place autofill work,  restricted to city

--- src/app/actnow/controllers/actnow-controller.js
+++ src/app/actnow/controllers/actnow-controller.js
@@ -9,38 +9,36 @@
 angular.module('acidification')
   .controller('actnowCtrl', function ($scope, $firebaseArray) {
 
+    $scope.newPlace = null;
+
+    $scope.autocompleteOptions = {
+      types: ['(cities)']
+    };
+
 
     $scope.newOAList = true;
     $scope.newCoalList = false;
     $scope.newLegList = false;
 
 
-    $scope.place = null;
-    $scope.autocompleteOptions = {
-      types: ['cities']
-    };
-
     var ref = new Firebase('https://oceanacidification.firebaseio.com/');
     $scope.emails = $firebaseArray(ref);
 
     // provide a method for adding an email
-    $scope.addEmail = function (newEmail, newFirstName, newLastName, newCity, newOAList, newCoalList, newLegList) {
+    $scope.addEmail = function (newEmail, newFirstName, newLastName, newPlace, newOAList, newCoalList, newLegList) {
       if (newEmail) {
         // push a message to the end of the array
         $scope.emails.$add({
           emailAddress: newEmail,
           firstName: newFirstName,
           lastName: newLastName,
-          city: newCity,
+          place: newPlace,
           OAList: newOAList,
           coalList: newCoalList,
           LegList: newLegList
-
         });
 
       }
     };
-
-
 
   });
