0a21760fb1377e7ea554b1333b1f4fa6eb292374
Lars Bell <larsbell@gmail.com>
2015-07-14 17:03:30 -0800

    adds analytics to module

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,6 @@
     'use strict';
 
   angular
-    .module('acidification', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'ngMaterial', 'google.places', 'firebase', 'com.2fdevs.videogular', 'com.2fdevs.videogular.plugins.controls', 'com.2fdevs.videogular.plugins.overlayplay', 'com.2fdevs.videogular.plugins.poster', 'google.places']);
+    .module('acidification', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'ngMaterial', 'google.places', 'angulartics', 'angulartics.google.analytics', 'firebase', 'com.2fdevs.videogular', 'com.2fdevs.videogular.plugins.controls', 'com.2fdevs.videogular.plugins.overlayplay', 'com.2fdevs.videogular.plugins.poster', 'google.places']);
 
 })();
